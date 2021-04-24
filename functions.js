27ffc58c28e12bf226b8544e3f9f65150cfdde13
MpedrosaB <mpedrosab@gmail.com>
2021-04-24 14:47:18 +0200

    Adding filter

--- functions.js
+++ functions.js
@@ -7,10 +7,18 @@ function sub(){
     var dppc = document.getElementById("cDPPC").checked;
     var chol = document.getElementById("cChol").checked;
         if (end==""){
-        end=
+        end="-"
     }
      alert (name+start+end+dppc+chol);
-
+var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("demo").innerHTML = this.responseText;
+  }
+        };
+    xmlhttp.open("GET", "functions.php?HEY=hey", true);
+xmlhttp.send();
+    
 };
     
 function createQuery(){
@@ -18,4 +26,8 @@ function createQuery(){
     
 };
    
+
+
+
+
 
