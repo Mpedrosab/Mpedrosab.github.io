e14ac33cc7371ff4113bdd47f66f01fa379af093
MpedrosaB <mpedrosab@gmail.com>
2021-04-24 09:43:55 +0200

    Adding filter

--- functions.js
+++ functions.js
@@ -1,16 +1,8 @@
+var product=null;
+
+var shelf=null;
 function sub(){
   product = document.getElementsByID("prod")[0].value;
   shelf = document.getElementsByID("shelf")[0].value;
-    var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "ParamDB.db",
-
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
 
 };
