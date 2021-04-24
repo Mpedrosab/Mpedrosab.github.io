103ac81e852d6de50e97ddec203cf1cd728f16f9
MpedrosaB <mpedrosab@gmail.com>
2021-04-24 13:04:29 +0200

    Adding filter

--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
 
-    alert ('¡Hola mundo!');
+
+function sub(){
+    var name = document.getElementById("name").value;
+    var date = document.getElementById("date").value;
+    var subs = document.getElementById("sub").value;
+     alert (name+date+subs);
+};
+   
 
