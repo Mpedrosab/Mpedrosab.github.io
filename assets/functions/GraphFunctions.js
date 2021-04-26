737ab607e5dc19ef8dbafbbac4f023b469bc238a
MpedrosaB <mpedrosab@gmail.com>
2021-04-26 20:25:30 +0200

    Comparison JSX

--- assets/functions/GraphFunctions.js
+++ assets/functions/GraphFunctions.js
@@ -1,75 +1,98 @@
 
-var brd = JXG.JSXGraph.initBoard('jxgbox',{boundingbox:[-1,5000,25,-400],axis:true});
-var color = ['blue','red','magenta', 'green', 'black','yellow'];
+
+/*Load Json at start*/
+var myArr;
+var arrayLength;
+var myArrData;
+var arrayDataLength;
+
+
+window.onload = function () {
+var xmlhttpData = new XMLHttpRequest();
+xmlhttpData.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+      console.log("Good");
+  myArrData = JSON.parse(this.responseText);
+  //myArrData = JSON.stringify(myArrData);
+  //myArrData = jQuery.parseJSON(myArrData);
+      arrayDataLength = Object.keys(myArrData).length;
+      console.log(myArrData);
+    console.log(Object.keys(myArrData));
+plotMyDB(myArrData['DPPC_B0420Y20']);
+plotMyDB(myArrData['Chol_B0414Y']);
+
+  }
+    else{
+        console.log ("Problem with JSON for data file!! =>Stataus: " +this.status );
+        
+    }
+};
+xmlhttpData.open("GET", "/Data/DataDB.json", true);
+xmlhttpData.send(); 
+
+
+};
+
+
+
+    var brd = JXG.JSXGraph.initBoard('jxgbox',{boundingbox:[-1,100,300,-10],axis:true});
+var color = ['blue','orange', 'green','red','magenta', 'black','yellow'];
 var nr = 0;
 var maxX = 0.0;
+var minX =  100000.0;
 var maxY = 0.0;
 var minY = 100000.0;
-
-function plotData(data) {
-    var i;
-    var x=data[0];
-    var x=data[1];
     
+    
+
+
+
+function splitData(myData){
+    myData=myData.replace("[","").replace("]","")
+    var splitArray=myData.split(",")
+    splitArray=splitArray.map(Number);
+    console.log(splitArray);
+    return splitArray;
+}
+
+
+function plotData(x,y) {
+    var i;
+    var x_arr = []; 
+    var y_arr = [];
     //var t = document.getElementById('jxgbox').value;
    // var data = t.split('\n');
+    var y0=y[0];
     for (i=0;i<x.length-1;i++) {
-        //d = data[i].split(';');
-        //x[i] = d[0]*1.0;
-        //y[i] = d[1]*1.0;
+        
+        
+        //Remove bias
+
+        y[i]=y[i]-y0;
         if (x[i]>maxX) maxX = x[i];
+        if (x[i]<minX) minX = x[i];
         if (y[i]>maxY) maxY = y[i];
         if (y[i]<minY) minY = y[i];
+        x_arr.push(x[i], x[i], NaN);
+        y_arr.push(y[i], y[i], NaN);
+        
+        
     }
-    brd.setBoundingBox([-1,maxY*1.01,maxX*1.05,minY*0.95]);
-    var c = brd.create('curve',[x,y],{strokeColor:color[nr%color.length]});
+    brd.setBoundingBox([-10,maxY+10,maxX+10,-10]);
+
+    //Scatter plot
+    var c = brd.create('curve', [x_arr, y_arr], {strokeWidth: 8,strokeColor:color[nr%color.length]})
+    brd.update();
+    
+    var d = brd.create('curve',[x,y],{strokeColor:color[nr%color.length],strokeWidth: 3});
     nr++;
     brd.update();
 };
 
-function loadData(value){
-    var myDiv= document.getElementById('hiddenData');
-   
-    myDiv.load("/"+value["HTML"]);
-   
-
-    /*
-    document.getElementById('hiddenData').innerHTML='<iframe class="myName" src="/myHTML" style="display:none"></iframe>'.replace("myHTML",value["HTML"]).replace("myName",value["Name"]);
-*/
+function plotMyDB(dataToPlot){
+    var x=splitData(dataToPlot["xData"]);
+    var y=splitData(dataToPlot["yData"]);
+    plotData(x,y);
 };
 
 
-function readData(value){
-        loadData(value); //Load the html in a hidden location
-    //var oTable = document.getElementsByClassName('dataframe')
-     var oTable = document.getElementsByClassName('dataframe isotherm_data')[0];
-
-    console.log(oTable)
-    //gets rows of table
-    var rowLength = oTable.rows.length;
-
-    //loops through rows    
-    for (i = 0; i < rowLength; i++){
-
-      //gets cells of current row  
-       var oCells = oTable.rows.item(i).cells;
-
-       //gets amount of cells of current row
-       var cellLength = oCells.length;
-
-       //loops through each cell in current row
-       for(var j = 0; j < cellLength; j++){
-
-              // get your cell info here
-
-              var cellVal = oCells.item(j).innerHTML;
-              alert(cellVal);
-           }
-    }
-    
-}
-
-var value={}
-value["HTML"]="Data/Chol_B0423Xss/Chol_B0423Xss_tuned.html";
-value["Name"]="n";
-readData(value);
